7a845c7e4bcb5d1574b21c9ee68bea5aa612c23f
Caue Polimanti <caue.polimanti@gmail.com>
2017-02-19 22:13:04 -0300

    Created Main and VistaCapital activities

---
+++ ContadorCapital/MainActivity.cs
@@ -0,0 +1,62 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+
+namespace ContadorCapital
+{
+	[Activity(Label = "ContadorCapital", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		double ingressosC, ingressosM, egressosC, egressosM, capitalC, capitalM;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button>(Resource.Id.btncalcular);
+
+			TextView txtIngressoColombia = FindViewById<TextView>(Resource.Id.txtincolombia);
+			TextView txtEgressoColombia = FindViewById<TextView>(Resource.Id.txtegcolombia);
+			TextView txtIngressoMexico = FindViewById<TextView>(Resource.Id.txtingmexico);
+			TextView txtEgressoMexico = FindViewById<TextView>(Resource.Id.txtegmexico);
+
+			button.Click += delegate {
+				try {
+					ingressosC = double.Parse(txtIngressoColombia.Text);
+					egressosC = double.Parse(txtEgressoColombia.Text);
+					ingressosM = double.Parse(txtIngressoMexico.Text);
+					egressosM = double.Parse(txtEgressoMexico.Text);
+
+					capitalC = ingressosC - egressosC;
+					capitalM = ingressosM - egressosM;
+
+					Carregar();
+				}
+				catch (System.Exception ex){
+					Toast.MakeText(this, ex.Message, ToastLength.Short).Show();
+				}
+			};
+		}
+
+		public void Carregar()
+		{
+			//instatiating an Intent object to start a VistaCapital activity
+			Intent objIntent = new Intent(this,
+										  typeof(VistaCapital));
+
+			//setting the content that is going to be sent to the next activity
+			objIntent.PutExtra("CapitalC", capitalC);
+			objIntent.PutExtra("CapitalM", capitalM);
+
+			//starting the VistaCapital activity
+			StartActivity(objIntent);
+		}
+	}
+}
+
---
+++ ContadorCapital/VistaCapital.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ContadorCapital
+{
+	[Activity(Label = "VistaCapital")]
+	public class VistaCapital : Activity
+	{
+		double defaultValue;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Create your application here
+			SetContentView(Resource.Layout.VistaCapital);
+
+			//find views
+			EditText txtCapitalM = FindViewById<EditText>(Resource.Id.txtcapmexico);
+			EditText txtCapitalC = FindViewById<EditText>(Resource.Id.txtcapcolombia);
+			ImageView imageMex = FindViewById<ImageView>(Resource.Id.imageMexico);
+			ImageView imageCol = FindViewById<ImageView>(Resource.Id.imageColombia);
+			Button btnSair = FindViewById<Button>(Resource.Id.btnsair);
+
+
+			try
+			{
+				txtCapitalM.Text = Intent.GetDoubleExtra("CapitalM", defaultValue).ToString();
+				txtCapitalC.Text = Intent.GetDoubleExtra("CapitalC", defaultValue).ToString();
+				imageMex.SetImageResource(Resource.Drawable.mexico);
+				imageCol.SetImageResource(Resource.Drawable.colombia);
+
+			}
+			catch (System.Exception ex) {
+				Toast.MakeText(this, ex.Message, ToastLength.Short).Show();
+			}
+
+			btnSair.Click += delegate {
+				Android.OS.Process.KillProcess(Android.OS.);
+			};
+
+
+		}
+	}
+}
